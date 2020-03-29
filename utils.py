a4e7ca67a08f0a3637fee961990b38b6398c7075
teganmaharaj <tegan.jrm@gmail.com>
2020-03-29 14:20:09 -0400

    Added description and keyword tag to _get_random_age

--- utils.py
+++ utils.py
@@ -5,14 +5,16 @@ import datetime
 def _normalize_scores(scores):
     return np.array(scores)/np.sum(scores)
 
-# should be a reasonable approximation for Canadian demographics
+# &canadian-demgraphics
 def _get_random_age():
+	# random normal centered on 50 with stdev 25
 	draw = np.random.normal(50, 25, 1)
 	if draw < 0:
+		# if below 0, shift to a bump centred around 30
 		age = round(30 + np.random.normal(0, 4))
 	else:
 		age = round(float(draw))
-	return draw
+	return age
 
 def _draw_random_discreet_gaussian(avg, scale):
     # https://stackoverflow.com/a/37411711/3413239
