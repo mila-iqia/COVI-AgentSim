13c900f5086a1e4992a4cdca923514bc85307d78
Shanya Sharma <sharmashanya1297@gmail.com>
2020-04-05 15:39:08 +0530

    Added changes to make distance a function of area and n_humans

--- base.py
+++ base.py
@@ -57,12 +57,13 @@ class City(object):
 class Location(simpy.Resource):
 
     def __init__(self, env, capacity=simpy.core.Infinity, name='Safeway', location_type='stores', lat=None, lon=None,
-                 cont_prob=None):
+                 area=None, cont_prob=None):
         super().__init__(env, capacity)
         self.humans = set()
         self.name = name
         self.lat = lat
         self.lon = lon
+        self.area = area
         self.location_type = location_type
         self.cont_prob = cont_prob
 
