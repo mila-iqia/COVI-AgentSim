6cd58eafbba9147219f5f9e1c73c1eebd9fbc795
Martin Weiss <martin.clyde.weiss@gmail.com>
2020-04-10 12:03:58 -0400

    added privacy/message cluster plotting

--- plots/privacy_plots.py
+++ plots/privacy_plots.py
@@ -6,6 +6,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 from utils import _decode_message
 from collections import defaultdict, Counter
+import networkx as nx
 
 contact_histories = json.load(open('contact_histories.json', 'r'))
 all_groups = []
@@ -46,30 +47,60 @@ for group in all_groups:
     all_count_people_in_group.extend(count_people_in_group)
 # import pdb; pdb.set_trace()
 
-fig, ax = plt.subplots(1, 1)
+plt.figure(1)
 plt.hist(np.array(all_count_people_in_group).flatten(), 100, label='num_people_in_group')
 plt.xlabel("Number of Messages in a Group")
 plt.ylabel("Frequency Count of Messages")
 plt.title("Histogram of Group Size")
 plt.savefig("plots/group_size_hist.png")
 
-fig, ax = plt.subplots(1, 1)
+plt.figure(2)
 plt.hist(np.array(all_within_group_percentage).flatten(), 50, label='unobs message ids in group')
 plt.xlabel("% of messages from most frequent unobs id")
 plt.ylabel("Frequency Count of Outcome")
 plt.title("Histogram of Group Dominant unobs id")
 plt.savefig("plots/within_group_percentage.png")
 
-fig, ax = plt.subplots(1, 1)
+plt.figure(3)
 plt.hist(np.array(all_number_of_groups).flatten(), 10, label='groups per user')
 plt.xlabel("number of groups")
 plt.ylabel("people with that number of groups")
 plt.title("Histogram of person <> group number frequency")
 plt.savefig("plots/group_number_freq.png")
 
-fig, ax = plt.subplots(1, 1)
+plt.figure(4)
 plt.hist(np.array(all_total_num_contacts).flatten(), 10, label='messages per user')
 plt.xlabel("number of messages")
 plt.ylabel("people with that number of messages")
 plt.title("Histogram of person <> message number frequency")
 plt.savefig("plots/message_number_freq.png")
+plt.clf()
+
+def hash_uid(group, uid, idx):
+    return str(group) + "-" + str(uid) + "-" + str(idx)
+
+for group_idx, groups in enumerate(all_groups):
+    G = nx.Graph()
+    all_uids = set()
+    for group, uids in groups.items():
+        for idx, uid in enumerate(uids):
+            G.add_node(hash_uid(group, uid, idx))
+            all_uids.add(uid)
+        for idx1, uid1 in enumerate(uids):
+            for idx2, uid2 in enumerate(uids):
+                # if idx2 > len(uids)/2:
+                #     continue
+                G.add_edge(hash_uid(group, uid1, idx1), hash_uid(group, uid2, idx2))
+    plt.figure(4+group_idx, figsize=(12,12))
+
+    node_color = []
+    for node in G.nodes():
+        node_color.append(int(node.split("-")[1]))
+    pos = nx.spring_layout(G, weight=.3, k=0.5, iterations=50)
+
+    nx.draw(G, pos, with_labels=True, node_color=node_color, cmap=plt.cm.hsv)
+    # plt.show()
+    plt.savefig(f"plots/groups/group_{group_idx}.png", dpi=300)
+    if group_idx > 10:
+        break
+
