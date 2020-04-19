91c69b08595c6c366125b10c86f8154c1168ca50
Martin Weiss <martin.clyde.weiss@gmail.com>
2020-04-19 08:12:10 -0400

    serialized log reads for reduced RAM consuption

--- models/dummy_human.py
+++ models/dummy_human.py
@@ -18,7 +18,7 @@ class DummyHuman:
         self.rng = rng
         self.all_reported_symptoms = [[]]
         self.all_symptoms = []
-        self.start_risk = np.log(0.)
+        self.start_risk = np.log(0.01)
         self._uid = None
         self.time_of_recovery = datetime.datetime.max
         self.infectiousness_start_time = datetime.datetime.max
