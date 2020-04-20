a8dc6c4978c186cd1b1bb9d0c0a752e554dabdf8
Martin Weiss <martin.clyde.weiss@gmail.com>
2020-04-20 12:08:46 -0400

    fixed merging

--- models/merge_outputs.py
+++ models/merge_outputs.py
@@ -5,17 +5,21 @@ import argparse
 
 parser = argparse.ArgumentParser(description='Run Risk Models and Plot results')
 parser.add_argument('--data_path', type=str, default="output/data.pkl")
+parser.add_argument('--output_path', type=str, default="output/data.pkl")
 args = parser.parse_args()
 all_data_for_day = []
-with zipfile.ZipFile(f"{args.data_path}.zip", mode='a', compression=zipfile.ZIP_STORED) as zf:
+with zipfile.ZipFile(f"{args.output_path}", mode='a', compression=zipfile.ZIP_STORED) as zf:
     for day_path in os.listdir(args.data_path):
         days_data = []
         data_dir_path = os.path.join(args.data_path, day_path)
-        for pkl in os.listdir(data_dir_path):
-            with open(os.path.join(data_dir_path, pkl, "daily_human.pkl"), 'rb') as f:
-                data = pickle.load(f)
-                zf.writestr(f"{day_path}-{pkl}.pkl", pickle.dumps(data))
-
+        try:
+            for pkl in os.listdir(data_dir_path):
+                with open(os.path.join(data_dir_path, pkl, "daily_human.pkl"), 'rb') as f:
+                    data = pickle.load(f)
+                    zf.writestr(f"{day_path}-{pkl}.pkl", pickle.dumps(data))
+        except Exception:
+            print(data_dir_path)
+            print(os.path.join(data_dir_path, pkl, "daily_human.pkl"))
 # add risks for plotting
 #     todays_date = start + datetime.timedelta(days=current_day)
 #     daily_risks.extend([(np.e ** human.risk, human.is_infectious(todays_date)[0], human.name) for human in hd.values()])
