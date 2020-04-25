dea6b98b6637e8aaad9ef3e3092cc78b6a8e8c26
Olexa Bilaniuk <obilaniu@gmail.com>
2020-04-25 02:46:47 -0400

    Begin engineering work to optimize the simulator.

---
+++ setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name                 = "covid19sim",
+    version              = "0.0.0.dev0",
+    url                  = "https://github.com/pg2455/covid_p2p_simulation",
+    description          = "Simulation of COVID-19 spread.",
+    long_description     = "Simulation of COVID-19 spread.",
+    classifiers          = [
+        "Development Status :: 1 - Planning",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3 :: Only",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    zip_safe             = False,
+    python_requires      = '>=3.7.4',
+    install_requires     = [
+        "scipy>=1.2.0",
+        "numpy>=1.16",
+        "simpy>=3.0.12,<4",
+        "matplotlib>=3.0",
+        "ipython>=7.10",
+        "click>=7.1.1",
+        "orderedset>=2.0.3",
+        "networkx==2.4",
+        "tqdm>=4.45.0",
+        "seaborn==0.10.0",
+    ],
+    packages             = find_packages("src"),
+    package_dir          = {'': 'src'},
+)
