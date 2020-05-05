7f930daa0a60433c04cf5b8f975ccb7b7026ad69
Martin Weiss <martin.clyde.weiss@gmail.com>
2020-05-05 00:20:05 -0400

    added README to output datasets

--- src/covid19sim/batch.sh
+++ src/covid19sim/batch.sh
@@ -10,6 +10,8 @@ init_percent_sick=0.01
 simulation_days=2
 n_jobs=10
 num_seeds=1
+sim_git_hash=$(cd covid_p2p_simulation; git rev-parse HEAD)
+ctt_git_hash=$(cd ctt; git rev-parse HEAD)
 
 for (( i=0; i<$num_seeds; i++ ))
   do
@@ -33,6 +35,23 @@ for (( i=0; i<$num_seeds; i++ ))
 
 # Copy the experimental config into the output zip
 cp $config_dir/$config_file $root_path/$config_file
+
+# Add a dataset README
+cat > README.txt <<EOL
+Simulator Git Commit Hash: ${sim_git_hash}
+Risk Prediction Git Commit Hash: ${ctt_git_hash}
+This dataset is structured as follows:
+- each directory contains the data for an experiment. The experiments differ only by random seed.
+- Within that directory, there is data contained in zip files.
+- Each zip archive contains the data for 1,000 people over the course of the mobility simulation. If there are only 1k people, then there is 1 zip archive.
+- The zip archive for 1k people for 1 experiment is structured as follows:
+- for each day, there is a directory, for each person there is a subdirectory containing a number of pickle files equal to the number of times that human updated their risk.
+- In that pickle file, we have data in a format specified in the docs/models.md of this repo: https://github.com/pg2455/covid_p2p_simulation/blob/develop/docs/models.md
+Essentially each "sample" here is the data on specific phone on a specific day. The task is to predict the unobserved variables from the observed variables. The most important unobserved variable is that person's infectiousness over the last 14 days. This variable contains information about the risk of exposure for this person with respect to their contacts. As input to your model, you have information about their encounters, whether this person got a test result, and what their symptoms are (if any). Most of these are structured as a rolling numpy array over the last 14 days.
+If you have any questions or you think you've found a bug -- please reach out to martin.clyde.weiss@gmail.com or post an issue to the relevant repository:
+- https://github.com/pg2455/covid_p2p_simulation
+- https://github.com/mila-iqia/covid_p2p_risk_prediction
+EOL
 
 # Cleanup the output
 rm -fr $root_path/$batch_path
