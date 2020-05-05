76ba5fb7c564b0b55bba81d19295a4a96cc0d9e0
Satya Ortiz-Gagne <satya.ortiz-gagne@mila.quebec>
2020-05-05 09:34:02 -0400

    Explain how requirements.txt is used in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 with open('requirements.txt', 'r') as f:
-    requirements = [line.strip() for line in f.readlines()]
+    requirements = [line.strip() for line in f.readlines() if not line.startswith("#")]
 
 setup(
     name                 = "covid19sim",
