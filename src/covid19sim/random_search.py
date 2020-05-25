ebff78ed664822636f31ecd924dee8b0402556fd
vict0rsch <vsch@pm.me>
2020-05-25 09:31:37 -0400

    add configurable sbatch files from template

--- src/covid19sim/random_search.py
+++ src/covid19sim/random_search.py
@@ -1,7 +1,7 @@
 import os
 import subprocess
 from pathlib import Path
-
+import tempfile
 import hydra
 import numpy as np
 import yaml
@@ -54,6 +54,59 @@ def sample_search_conf(exp):
     return conf
 
 
+def load_template():
+    with (Path(__file__).parent / "job_template.txt").open("r") as f:
+        return f.read()
+
+
+def fill_template(template_str, conf):
+    user = os.environ.get("USER")
+    home = os.environ.get("HOME")
+
+    partition = conf.get("partition", "main")
+    cpu = conf.get("cpu", 6)
+    mem = conf.get("mem", 16)
+    gres = conf.get("gres", "")
+    time = conf.get("time", "4:00:00")
+    log = conf.get("log", f"/network/tmp1/{user}/covi-slurm-%j.out")
+    env_name = conf.get("env_name", "covid")
+    code_loc = conf.get("code_loc", str(Path(home) / "simulator/src/covid19sim/"))
+
+    if "dev" in conf and conf["dev"]:
+        print(
+            "Using:\n"
+            + "\n".join(
+                [
+                    "  {:10}: {}".format("partition", partition),
+                    "  {:10}: {}".format("cpu", cpu),
+                    "  {:10}: {}".format("mem", mem),
+                    "  {:10}: {}".format("gres", gres),
+                    "  {:10}: {}".format("time", time),
+                    "  {:10}: {}".format("log", log),
+                    "  {:10}: {}".format("env_name", env_name),
+                    "  {:10}: {}".format("code_loc", code_loc),
+                ]
+            )
+        )
+
+    partition = f"#SBATCH --partition={partition}"
+    mem = f"#SBATCH --mem={mem}GB"
+    gres = f"#SBATCH --gres={gres}"
+    time = f"#SBATCH --time={time}"
+    log = f"#SBATCH -o {log}"
+
+    return template_str.format(
+        partition=partition,
+        cpu=cpu,
+        mem=mem,
+        gres=gres,
+        time=time,
+        log=log,
+        env_name=env_name,
+        code_loc=code_loc,
+    )
+
+
 @hydra.main(config_path="hydra-configs/search/config.yaml", strict=False)
 def main(conf: DictConfig) -> None:
 
@@ -72,7 +125,19 @@ def main(conf: DictConfig) -> None:
     """
 
     # These will be filtered out when passing arguments to run.py
-    RANDOM_SEARCH_SPECIFIC_PARAMS = {"n_search", "dev", "exp_file"}
+    RANDOM_SEARCH_SPECIFIC_PARAMS = {
+        "n_search",
+        "dev",
+        "exp_file",
+        "partition",
+        "cpu",
+        "mem",
+        "time",
+        "log",
+        "gres",
+        "env_name",
+        "code_loc",
+    }
 
     # move back to original directory because hydra moved
     os.chdir(hydra.utils.get_original_cwd())
@@ -83,33 +148,56 @@ def main(conf: DictConfig) -> None:
     # load experimental configuration
     # override with exp_file=<X>
     # where <X> is in hydra-configs/search and is ".yaml"
-    exp = load_search_conf(
+    conf = load_search_conf(
         Path()
         / "hydra-configs"
         / "search"
         / (overrides.get("exp_file", "experiment") + ".yaml")
     )
-    # override experimental parametrization with the commandline args
-    exp.update(overrides)
+    # override experimental parametrization with the commandline conf
+    conf.update(overrides)
+
+    template_job_str = load_template()
 
     # run n_search jobs
     print("-" * 80)
     print("-" * 80)
     for i in range(conf.get("n_search", 1)):
         print("\nJOB", i)
-        opts = sample_search_conf(exp)
-        command = "sbatch job.sh"
-        for k, v in opts.items():
-            if k not in RANDOM_SEARCH_SPECIFIC_PARAMS:
-                command += f" {k}={v}"
-        if "dev" in exp and exp["dev"]:
-            print(">>> ", command)
-        else:
-            process = subprocess.call(command.split())
-        print()
-        print("-" * 80)
-        print("-" * 80)
 
+        # sample parameters
+        opts = sample_search_conf(conf)
+        # fill-in template with `partition` `time` `code_loc` etc. from command-line overwrites
+        job_str = fill_template(template_job_str, conf)
+        # get temporary file to write sbatch run file
+        tmp = Path(tempfile.NamedTemporaryFile(suffix='.sh').name)
+
+        try:
+            # create temporary sbatch file
+            with tmp.open("w") as f:
+                f.write(job_str)
+
+            # Base command: sbatch tmp_file.sh
+            command = f"sbatch {str(tmp)}"
+            # Add covid19sim/run.py hydra arguments
+            for k, v in opts.items():
+                if k not in RANDOM_SEARCH_SPECIFIC_PARAMS:
+                    command += f" {k}={v}"
+            # dev-mode: don't actually run the command
+            if "dev" in conf and conf["dev"]:
+                print(">>> ", command)
+            else:
+                # not dev-mode: sbatch it!
+                process = subprocess.call(command.split())
+
+            # prints
+            print()
+            print("-" * 80)
+            print("-" * 80)
+        finally:
+            print()
+            # remove trailing sbatch file
+            os.remove(tmp)
 
 
 if __name__ == "__main__":
