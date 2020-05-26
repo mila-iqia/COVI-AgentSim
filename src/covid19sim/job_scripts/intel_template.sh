00525a25dec90bd5d166ceaa1be0ebc732d27671
vict0rsch <vsch@pm.me>
2020-05-26 10:00:50 -0400

    gdb

--- src/covid19sim/job_scripts/intel_template.sh
+++ src/covid19sim/job_scripts/intel_template.sh
@@ -8,6 +8,6 @@ echo $(which python)
 echo "Stating Job"
 
 
-# python server_bootstrap.py -e {weights} -w 4 > {server_out} 2>&1 &
+gdb -ex r -ex "thread apply all bt" --args python server_bootstrap.py -e {weights} -w 4 > {server_out} 2>&1 &
 
 # DO NOT WRITE ANYTHIN AFTER THIS, PYTHON WILL APPEND TO THIS FILE
