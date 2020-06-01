7b1d8670672f119e357ed98db30c5e21a88169d3
vict0rsch <vsch@pm.me>
2020-06-01 18:51:52 -0400

    dummy metrics

--- src/covid19sim/other/eval_transformer.py
+++ src/covid19sim/other/eval_transformer.py
@@ -3,6 +3,27 @@ import argparse
 import pickle
 
 
+def compute_early_warning(data) -> dict:
+    pass  # TODO
+
+
+def compute_true_false_quarantine(data) -> tuple:
+    pass  # TODO
+
+
+def compute_exposed_to_infectious_interval(data) -> float:
+    pass  # TODO
+
+
+def get_metrics(data) -> dict:
+    # early warning: how much time after exposure put in non-green bucket
+    early_warning_per_rec_level: dict = compute_early_warning(data)
+    # red but not even exposed (not infected nor infectious) and red and truely infectious
+    true_quarantine, false_quarantine = compute_true_false_quarantine(data)
+    # was the rec_level increased between exposure and infectiousness (good) or not (bad)
+    exposed_to_infectious_interval = compute_exposed_to_infectious_interval(data)
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--dir", type="str", default=".", help="experimental directory")
@@ -11,12 +32,10 @@ if __name__ == "__main__":
     path = Path(opts.path).resolve()
     model = path.parent.name
 
-    candidates = [d for d in path.iterdir() if d.is_dir()]
+    runs = [d for d in path.iterdir() if d.is_dir()]
 
-    for c in candidates:
-        data_path = list(c.glob("*.pkl"))[0]
+    for run in runs:
+        data_path = list(run.glob("tracker*.pkl"))[0]
         with data_path.open("rb") as f:
             data = pickle.load(f)
-
-
-# DASHING-UNIVERSE vs BDT
+            metrics: dict = get_metrics(data)
