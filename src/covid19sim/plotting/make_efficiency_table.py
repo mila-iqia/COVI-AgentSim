6a4350e08f61dd09432f12c147df798d9dcb045e
Martin Weiss <martin.clyde.weiss@gmail.com>
2020-07-09 10:55:51 -0400

    added efficiency table

---
+++ src/covid19sim/plotting/make_efficiency_table.py
@@ -0,0 +1,138 @@
+import math
+from pathlib import Path
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from matplotlib.lines import Line2D
+
+from covid19sim.plotting.utils import (
+    get_all,
+    get_metrics,
+)
+
+def run(data, path, comparison_key):
+    """
+    data is a dictionnary that maps methods (bdt1, bdt1_norm, transformer etc.)
+    to another dictionnary which has keys the values of the comparing key and
+    values a dictionnary with the run's simulation configuration and pkl path
+
+    e.g.
+    comparison_key=APP_UPTAKE
+    data:
+      bdt1:
+        -1:
+          pkl: loaded_tracker.pkl
+          conf: configuration_dict
+        0.8415:
+          pkl: loaded_tracker.pkl
+          conf: configuration_dict
+      bdt2:
+        -1:
+          pkl: loaded_tracker.pkl
+          conf: configuration_dict
+        0.8415:
+          pkl: loaded_tracker.pkl
+          conf: configuration_dict
+
+
+    Args:
+        data (dict): the data as method -> comparing value -> conf, pkl
+        comparison_key (str): the key used to compare runs, like APP_UPTAKE
+    """
+    print("Preparing data...")
+    pkls = []
+    labels = []
+    pkls_norm = []
+    labels_norm = []
+
+    for method in data:
+        for key in data[method]:
+            if "_norm" in method:
+                pkls_norm.append([r["pkl"] for r in data[method][key].values()])
+                labels_norm.append(f"{method}_{key}")
+            else:
+                pkls.append([r["pkl"] for r in data[method][key].values()])
+                labels.append(f"{method}_{key}")
+
+    for idx, label in enumerate(labels):
+        print(f"{len(pkls[idx])} seeds for {label}")
+
+    rows = get_all(pkl_types=pkls, labels=labels, normalized=False)
+    lrows = set([r[0] for r in rows])
+    labels = [label for label in labels if label in lrows]
+
+    rows_norm = get_all(pkl_types=pkls_norm, labels=labels_norm, normalized=True)
+    lrows_norm = set([r[0] for r in rows_norm])
+    labels_norm = [label for label in labels_norm if label in lrows_norm]
+
+    rows = rows + rows_norm
+
+    n_seeds = None
+    for mv in data.values():
+        for cv in mv.values():
+            n_seeds = len(cv)
+            break
+        break
+    assert n_seeds is not None, "Could not find the number of seeds"
+    df = pd.DataFrame(rows, columns=["type", "metric"] + list(np.arange(n_seeds) + 1))
+    df["mean"] = df[list(np.arange(n_seeds) + 1)].mean(axis=1)
+    df["stderr"] = df[list(np.arange(n_seeds) + 1)].sem(axis=1)
+
+    ############
+    ### /!\ Ordering should be consistent everywhere. i.e. _70, _60, _40
+    ############
+
+    xmetrics = [
+        "f3",
+        "f2",
+        "f1",
+        "f2_up",
+        "effective_contacts",
+        "outside_daily_contacts",
+    ]
+    ymetric = "percent_infected"
+
+    colormap = [
+        "mediumvioletred",
+        "darkorange",
+        "green",
+        "red",
+        "blue",
+        "brown",
+        "cyan",
+        "gray",
+        "olive",
+        "orange",
+        "pink",
+        "purple",
+        "royalblue",
+    ]
+
+    fig, axs = plt.subplots(
+        nrows=math.ceil(len(xmetrics) / 2),
+        ncols=2,
+        figsize=(20, 20),
+        dpi=100,
+        sharey=True,
+    )
+
+    base_methods = sorted(data.keys())
+
+    results = []
+    print("| Method | % Rec Level 3 | % Rec Level 2 | % Rec Level 1 | % Infected |")
+    print("|---|---|---|---|---|")
+    for idx, method in enumerate(sorted(base_methods)):
+        current_labels = sorted([lab for lab in labels if lab.startswith(method)])
+        for i, lab in enumerate(current_labels):
+            result = f"{lab} | "
+
+            for axis_idx, xmetric in enumerate(xmetrics):
+                if xmetric in ["f1", "f2", "f3"]:
+                    x, xe = get_metrics(df, lab, xmetric)
+                    y, ye = get_metrics(df, lab, ymetric)
+                    result += f" {round(float(x)*100, 1)}% +/- {round(float(xe*100), 1)}% | "
+            infected = f" {round(float(y * 100), 1)}% +/- {round(float(ye * 100), 1)}% |"
+            result += infected
+            print(result)
+            results.append(result)
--- src/covid19sim/plotting/plot_pareto_adoption.py
+++ src/covid19sim/plotting/plot_pareto_adoption.py
@@ -1,165 +1,16 @@
 import math
-import pickle
 from pathlib import Path
 
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from matplotlib.lines import Line2D
-from scipy import stats as sps
 
 from covid19sim.plotting.utils import (
-    get_data,
-    get_human_rec_levels,
-    get_human_states,
-    get_title
+    get_title,
+    get_all,
+    get_metrics,
 )
-
-
-def get_all_false(filename=None, data=None, normalized=False):
-    data = get_data(filename, data)
-    intervention_day = data["intervention_day"]
-    if intervention_day < 0:
-        intervention_day = 0
-    states = get_human_states(data=data)
-    states = states[:, intervention_day:]
-    rec_levels = get_human_rec_levels(data=data, normalized=normalized)
-
-    false_level3 = np.sum(((states == 0) | (states == 3)) & (rec_levels == 3), axis=0)
-    false_level2 = np.sum(((states == 0) | (states == 3)) & (rec_levels == 2), axis=0)
-    false_level1 = np.sum(((states == 0) | (states == 3)) & (rec_levels == 1), axis=0)
-    false_level1_above = np.sum(
-        ((states == 0) | (states == 3))
-        & ((rec_levels == 1) | (rec_levels == 2) | (rec_levels == 3)),
-        axis=0,
-    )
-    false_level2_above = np.sum(
-        ((states == 0) | (states == 3)) & ((rec_levels == 2) | (rec_levels == 3)),
-        axis=0,
-    )
-    return (
-        false_level3 / states.shape[0],
-        false_level2 / states.shape[0],
-        false_level1 / states.shape[0],
-        false_level1_above / states.shape[0],
-        false_level2_above / states.shape[0],
-    )
-
-
-def get_proxy_r(data):
-    total_infected = 0
-    for k in data["humans_state"].keys():
-        total_infected += any(z == "I" for z in data["humans_state"][k][5:])
-    return sum(data["cases_per_day"][5:]) / total_infected
-
-
-def get_fq_r(filename=None, data=None, normalized=False):
-    assert filename is not None or data is not None
-    if data is None:
-        data = pickle.load(open(filename, "rb"))
-
-    f3, f2, f1, f1_up, f2_up = get_all_false(data=data, normalized=normalized)
-    x = [i[-5:].mean() for i in [f3, f2, f1, f1_up, f2_up]]
-
-    intervention_day = data["intervention_day"]
-    od = np.mean(data["outside_daily_contacts"][intervention_day:])
-    ec = data["effective_contacts_since_intervention"]
-
-    # percent_infected
-    y = sum(data["cases_per_day"]) / data["n_humans"]
-
-    # R
-    z = get_effective_R(data)
-
-    # proxy_r
-    a = get_proxy_r(data)
-
-    return x, y, z, a, od, ec
-
-
-def get_mean_fq_r(filenames=None, pkls=None, normalized=False):
-    assert filenames is not None or pkls is not None
-    if pkls is not None:
-        _tmp = [(None, {"pkl": pkl}) for pkl in pkls]
-    elif filenames is not None:
-        _tmp = [(filename, None) for filename in filenames]
-    else:
-        raise ValueError("filenames and pkls are None")
-
-    metrics = {
-        "f3": [],
-        "f2": [],
-        "f1": [],
-        "f1_up": [],
-        "f2_up": [],
-        "percent_infected": [],
-        "r": [],
-        "proxy_r": [],
-        "outside_daily_contacts": [],
-        "effective_contacts": [],
-    }
-    for filename, pkl in _tmp:
-        x, y, z, a, od, ec = get_fq_r(
-            filename=filename, data=pkl["pkl"], normalized=normalized
-        )
-        metrics["f3"].append(x[0])
-        metrics["f2"].append(x[1])
-        metrics["f1"].append(x[2])
-        metrics["f1_up"].append(x[3])
-        metrics["f2_up"].append(x[4])
-        metrics["percent_infected"].append(y)
-        metrics["r"].append(z)
-        metrics["proxy_r"].append(a)
-        metrics["outside_daily_contacts"].append(od)
-        metrics["effective_contacts"].append(ec)
-
-    return metrics
-
-
-def get_effective_R(data):
-    GT = data["generation_times"]
-    a = 4
-    b = 0.5
-    window_size = 5
-    ws = [sps.gamma.pdf(x, a=GT, loc=0, scale=0.9) for x in range(window_size)]
-    last_ws = ws[::-1]
-    cases_per_day = data["cases_per_day"]
-
-    lambda_s = []
-    rt = []
-    for i in range(len(cases_per_day)):
-        if i < window_size:
-            last_Is = cases_per_day[:i]
-        else:
-            last_Is = cases_per_day[(i - window_size) : i]
-
-        lambda_s.append(sum(x * y for x, y in zip(last_Is, last_ws)))
-        last_lambda_s = sum(lambda_s[-window_size:])
-        rt.append((a + sum(last_Is)) / (1 / b + last_lambda_s))
-    return np.mean(rt[-5:])
-
-
-def get_all(filename_types=None, pkl_types=None, labels=[], normalized=False):
-    if pkl_types is not None:
-        tmp = [(None, pkls) for pkls in pkl_types]
-    elif filename_types is not None:
-        tmp = [(filenames, None) for filenames in filename_types]
-    else:
-        raise ValueError("filename_types and pkl_types are None")
-
-    _rows = []
-
-    for i, (filenames, pkls) in enumerate(tmp):
-        metrics = get_mean_fq_r(filenames=filenames, pkls=pkls, normalized=normalized)
-        for key, val in metrics.items():
-            _rows.append([labels[i], key] + val)
-    return _rows
-
-
-def get_metrics(data, label, metric):
-    tmp = data[(data["type"] == label) & (data["metric"] == metric)]
-    return tmp["mean"], tmp["stderr"]
-
 
 def plot_all_metrics(
     axs,
