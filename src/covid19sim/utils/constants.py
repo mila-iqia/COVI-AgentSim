8d8a81038a5c286ac6618a7b4deba9e733c15b70
Prateek Gupta <pg2455@columbia.edu>
2020-08-21 10:52:01 -0400

    make all strings as constants to avoid confusion

--- src/covid19sim/utils/constants.py
+++ src/covid19sim/utils/constants.py
@@ -14,6 +14,36 @@ WEEKDAYS = [0, 1, 2, 3, 4]
 WEEKENDS = [5, 6]
 ALL_DAYS = [0, 1, 2, 3, 4, 5, 6]
 
+# triggers for taking test
+TAKE_TEST_DUE_TO_SELF_DIAGNOSIS = "self-diagnosis"
+TAKE_TEST_DUE_TO_RANDOM_REASON = "random"
+TAKE_TEST_DUE_TO_RECOMMENDATION = "recommended"
+
+# triggers for behavior changes
+TEST_TAKEN = "test-taken"
+SELF_DIAGNOSIS = "self-diagnosis"
+RISK_LEVEL_UPDATE = "risk-level-update"
+
+# test results
+NEGATIVE_TEST_RESULT = "negative"
+POSITIVE_TEST_RESULT = "positive"
+
+# quarantine behaviors
+QUARANTINE_UNTIL_TEST_RESULT = f"{TEST_TAKEN}-{NEGATIVE_TEST_RESULT}"
+QUARANTINE_DUE_TO_POSITIVE_TEST_RESULT = f"{TEST_TAKEN}-{POSITIVE_TEST_RESULT}"
+
+TRACED_BY_POSITIVE_TEST = "traced-positive-test"
+TRACED_BY_SELF_REPORTED_SYMPTOMS = "traced-self-reported"
+MAX_RISK_LEVEL_TRACED = "max-risk-traced"
+
+ALL_QUARANTINE_TRIGGERS = [QUARANTINE_UNTIL_TEST_RESULT, QUARANTINE_DUE_TO_POSITIVE_TEST_RESULT, SELF_DIAGNOSIS, TRACED_BY_SELF_REPORTED_SYMPTOMS, TRACED_BY_SELF_REPORTED_SYMPTOMS, MAX_RISK_LEVEL_TRACED]
+UNSET_QUARANTINE = "unset-quarantine"
+QUARANTINE_HOUSEHOLD = "quarantine-household"
+
+# other behaviors
+INITIALIZED_BEHAVIOR = "initialization"
+INTERVENTION_START_BEHAVIOR = "intervention_start"
+
 # See commentary in covid19sim.native._native.
 from covid19sim.native._native import (SECONDS_PER_MINUTE,
                                        SECONDS_PER_HOUR,
