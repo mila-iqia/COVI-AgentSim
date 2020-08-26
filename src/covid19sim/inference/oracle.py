1904424a4c220be3065145b361c10c44cf6bc06e
nrahaman <nasim.rahaman@tuebingen.mpg.de>
2020-08-26 14:44:47 +0200

    - additive noise for noisy oracle

--- src/covid19sim/inference/oracle.py
+++ src/covid19sim/inference/oracle.py
@@ -3,13 +3,17 @@ import numpy as np
 
 def oracle(human, conf):
     human_infectiousnesses = np.asarray(human.infectiousnesses)
-    if conf.get("ORACLE_NOISE") > 0:
+    if conf.get("ORACLE_MUL_NOISE") > 0 or conf.get("ORACLE_ADD_NOISE") > 0:
         rng = np.random.RandomState(human.oracle_noise_random_seed)
-        noise_mask = rng.uniform(-conf.get("ORACLE_NOISE"), conf.get("ORACLE_NOISE"),
-                                 size=human_infectiousnesses.shape)
-        noise_mask = 1 + noise_mask
+        mul_noise_mask = rng.uniform(-conf.get("ORACLE_MUL_NOISE"),
+                                     +conf.get("ORACLE_MUL_NOISE"),
+                                     size=human_infectiousnesses.shape)
+        mul_noise_mask = 1 + mul_noise_mask
+        add_noise_mask = rng.uniform(0., conf.get("ORACLE_ADD_NOISE", 0.),
+                                     size=human_infectiousnesses.shape)
     else:
-        noise_mask = 1
+        mul_noise_mask = 1
+        add_noise_mask = 0
     # return ground truth infectiousnesses
-    risk_history = (human_infectiousnesses * noise_mask).tolist()
+    risk_history = ((human_infectiousnesses + add_noise_mask) * mul_noise_mask).tolist()
     return risk_history
