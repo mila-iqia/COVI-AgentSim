7916edc37a9014c332b80419af1ba21c8e1f8c38
Prateek Gupta <pg2455@columbia.edu>
2020-08-26 11:06:06 -0400

    fix adding residents which was assuming earlier it as a set

--- src/covid19sim/locations/location.py
+++ src/covid19sim/locations/location.py
@@ -537,7 +537,7 @@ class Household(Location):
             human (covid19sim.human.Human): `human` who needs to be added to this house
             index_case_history (dict): current index case value for `human` in the previous house. Defaults to None.
         """
-        self.residents.add(human)
+        self.residents.append(human)
         if index_case_history:
             self.index_cases[human] = index_case_history
 
