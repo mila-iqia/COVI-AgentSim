9b16f00b0296d65bf73d8ad90310f50c0032103c
nrahaman <nasim.rahaman@tuebingen.mpg.de>
2020-09-14 20:00:43 +0200

    - no-space mode in raven launcher

--- src/covid19sim/job_scripts/raven_launcher.py
+++ src/covid19sim/job_scripts/raven_launcher.py
@@ -23,19 +23,29 @@ def parse_args():
         default=False,
         help="Whether to just print stuff.",
     )
+    parsey.add_argument(
+        "-s",
+        "--no-space",
+        action="store_true",
+        default=False,
+        help="Whether to get rid of spaces."
+    )
     # parsey.add_argument("-r", "--run", type=str, required=True, help="Run file.")
     return parsey.parse_args()
 
 
-def validate_components(components):
+def validate_components(components, no_space=False):
     components[-1] = components[-1].replace(";", "")
     for idx, component in enumerate(components):
         if " " in component:
-            # hydra doesn't like spaces without quotes, but shlex likes to get rid
-            # of the quotes. :|
-            assert "=" in component, "Can't parse hydra command with a space."
-            key, value = component.split("=")
-            components[idx] = f'{key}=\\"{value}\\"'
+            if no_space:
+                components[idx] = component.replace(" ", "")
+            else:
+                # hydra doesn't like spaces without quotes, but shlex likes to get rid
+                # of the quotes. :|
+                assert "=" in component, "Can't parse hydra command with a space."
+                key, value = component.split("=")
+                components[idx] = f'{key}=\\"{value}\\"'
     return components
 
 
