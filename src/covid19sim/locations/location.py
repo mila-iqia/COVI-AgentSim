63c039de6bc639eab7cdb817f52a67ce946f490d
Martin Weiss <martin.clyde.weiss@gmail.com>
2020-09-28 20:51:00 -0400

    adding exps

--- src/covid19sim/locations/location.py
+++ src/covid19sim/locations/location.py
@@ -423,10 +423,8 @@ class Location(simpy.Resource):
                                     p_infection=p_transmission,
                                     success=x_environment
                                 )
-        #
         if x_environment:
             human._get_infected(initial_viral_load=self.rng.random())
-            Event.log_exposed(self.conf.get('COLLECT_LOGS'), human, self, p_transmission, self.env.timestamp)
 
         return
 
