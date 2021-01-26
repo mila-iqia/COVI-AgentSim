442c95ea60fdfa8d9f5bf62e9f94f38c0629c08b
Prateek Gupta <pg2455@columbia.edu>
2021-01-26 11:50:03 +0000

    fix metrics

--- src/covid19sim/plotting/plot_normalized_mobility_scatter.py
+++ src/covid19sim/plotting/plot_normalized_mobility_scatter.py
@@ -29,9 +29,9 @@ LEGENDSIZE = 25
 ANNOTATION_FONTSIZE=15
 
 METRICS = ['r', 'false_quarantine', 'false_sr', 'effective_contacts', 'healthy_contacts', 'percentage_infected', \
-            'fraction_false_non_risky', 'fraction_false_risky', 'positivity_rate', 'fraction_quarantine', 'mobility_factor']
+            'fraction_false_non_risky', 'fraction_false_risky', 'positivity_rate', 'fraction_quarantine']
 
-SENSITIVITY_PARAMETERS = ['ASYMPTOMATIC_RATIO', 'ALL_LEVELS_DROPOUT', 'P_DROPOUT_SYMPTOM',  'PROPORTION_LAB_TEST_PER_DAY', 'BASELINE_P_ASYMPTOMATIC']# used for sensitivity plots
+SENSITIVITY_PARAMETERS = ['ASYMPTOMATIC_RATIO', 'ALL_LEVELS_DROPOUT', 'P_DROPOUT_SYMPTOM',  'PROPORTION_LAB_TEST_PER_DAY', 'BASELINE_P_ASYMPTOMATIC', 'GLOBAL_MOBILITY_SCALING_FACTOR']# used for sensitivity plots
 
 USE_MATH_NOTATION=False
 
